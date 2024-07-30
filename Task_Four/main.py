c97cac393c9d325e2ed6c0c0a0ffb6b26d9d67b9
Nikhil Narasimhan <117485953+nikhilmn7@users.noreply.github.com>
2024-07-30 14:55:55 -0700

    Code for Task 4

---
+++ Task_Four/main.py
@@ -0,0 +1,54 @@
+import os
+import random
+import re
+
+import numpy as np
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from cardiac_ml_tools import read_data_dirs
+from dataloader import ECGDataset
+from model import SqueezeNet1D
+from trainer import train
+from inference import inference
+
+if __name__ == "__main__":
+    writer = SummaryWriter("log/experiment13")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SqueezeNet1D().to(device)
+    data_dirs = []
+    regex = r'data_hearts_dd_0p2*'
+    DIR = '../intracardiac_dataset/'
+    for x in os.listdir(DIR):
+        if re.match(regex, x):
+            data_dirs.append(DIR + x)
+    file_pairs = read_data_dirs(data_dirs)
+    print('Number of file pairs: {}'.format(len(file_pairs)))
+    # example of file pair
+    print("Example of file pair:")
+    print("{}\n{}".format(file_pairs[0][0], file_pairs[0][1]))
+    print("{}".format(file_pairs[1]))
+
+    random.shuffle(file_pairs)
+
+    train_file_pairs = np.array(file_pairs[0:12893])
+    np.save('train.npy', train_file_pairs)
+
+    val_file_pairs = np.array(file_pairs[12893:14505])
+    np.save('val.npy', val_file_pairs)
+
+    test_file_pairs = np.array(file_pairs[14505:])
+    np.save('test.npy', test_file_pairs)
+
+    train_dataset = ECGDataset(np.load('train.npy'))
+    val_dataset = ECGDataset(np.load('val.npy'))
+
+    train_loader = DataLoader(train_dataset, batch_size=32, num_workers=20, shuffle=True, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, num_workers=20, pin_memory=True)
+
+    trained_model = train(writer, device, model, train_loader, val_loader, num_epochs=200, learning_rate=0.001)
+
+    writer.close()
